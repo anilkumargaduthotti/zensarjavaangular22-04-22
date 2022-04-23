0c9ef2b5a079cc252dcfecc7c6a533a0274ffebb
AnilKumar Gaduthotti <a.gaduthotti@zensar.com>
2022-04-23 21:12:00 +0530

    Practice1

---
+++ Basics/practice1.java
@@ -0,0 +1,18 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class practice1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name");
+        String name = sc.nextLine();
+        System.out.println("Enter a age");
+        int age = sc.nextInt();
+         if (age >= 18) {
+             System.out.println("Name is " + name + " age is " + age + " is an adult");
+         } else {
+             System.out.println("Name is " + name + " age is " + age + "is not an adult");
+         }
+    }
+}
